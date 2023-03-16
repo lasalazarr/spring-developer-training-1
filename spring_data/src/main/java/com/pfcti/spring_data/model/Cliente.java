f88a001709edcb1114be790fa2ddfaf9f5be274b
Max Acevedo Retana <max.acevedo@pfcti.com>
2023-03-16 17:54:16 -0600

    prueba

---
+++ spring_data/src/main/java/com/pfcti/spring_data/model/Cliente.java
@@ -0,0 +1,23 @@
+package com.pfcti.spring_data.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="cliente")
+@Setter
+@Getter
+public class Cliente {
+    @Id
+    private int Id;
+    @Column(name="nombre")
+    private String nombre;
+    @Column(length = 30)
+    private String apellidos;
+    @Column(columnDefinition = "varchar(15)")
+    private String cedula;
+    private String telefono;
+}
