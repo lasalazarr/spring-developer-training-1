4f45c4fa647c874cec670f45e2263376d202a5c8
Max Acevedo Retana <max.acevedo@pfcti.com>
2023-03-16 19:42:06 -0600

    Clase 2

--- spring_data/src/main/java/com/pfcti/spring_data/model/direccion.java
+++ spring_data/src/main/java/com/pfcti/spring_data/model/direccion.java
@@ -1,6 +1,7 @@
 package com.pfcti.spring_data.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
